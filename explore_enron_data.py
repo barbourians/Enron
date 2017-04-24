20d78c6d9eeb6214cb8b475d8ca390d4047a5f2c
barbourians <github@barbourians.org>
2017-04-24 10:33:01 +0200

    Answers to quiz 13 to 27

--- explore_enron_data.py
+++ explore_enron_data.py
@@ -17,6 +17,63 @@
 
 import pickle
 
-enron_data = pickle.load(open("C:\Udacity\machine-learning\projects\Enron\final_project\final_project_dataset.pkl", "r"))
+enron_data = pickle.load(open("\\Udacity\\machine-learning\\projects\\Enron\\final_project\\final_project_dataset.pkl", "r"))
+
+# Quiz questions
+# Q13 - How many data points (people) are in the dataset?
+print '13. Quiz: Size of the Enron Dataset?',len(enron_data)
+
+# Q14 - For each person, how many features are available?
+print '14. Quiz: Features in the Enron Dataset?',len(enron_data["SKILLING JEFFREY K"])
+
+# Q15 - How many POIs are there in the E+F dataset?
+count = 0
+for person in enron_data:
+    if enron_data[person]["poi"]==True:
+        count+=1
+print '15. Quiz: Finding POIs in the Enron Data?',count
+
+# Q16 - How many POI's were there total?
+poi_names = open("\\Udacity\\machine-learning\\projects\\Enron\\final_project\\poi_names.txt", "r")
+count = 0
+while 1:
+    line=poi_names.readline() # check for EOF
+    if not line: break
+    if line[0:3] == '(y)' or line[0:3] == '(n)':
+        count+=1
+poi_names.close()
+print '16. Quiz: POIs:',count
+
+# Q18 What is the total value of the stock belonging to James Prentice?
+print '18. Quiz: Value of the stock belonging to James Prentice:',enron_data['PRENTICE JAMES']["total_stock_value"]
+
+# Q19 How many email messages do we have from Wesley Colwell to persons of interest?
+print '19. Quiz: Email messages from Wesley Colwell to POI:',enron_data['COLWELL WESLEY']["from_this_person_to_poi"]
+
+# Q20 What's the value of stock options exercised by Jeffrey K Skilling?
+print '19. Quiz: Stock options exercised by Jeffrey K Skilling:',enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+# Q25 Of these three individuals (Lay, Skilling and Fastow), who took home the most money?
+print '25. Quiz: SKILLING received:',enron_data["SKILLING JEFFREY K"]["total_payments"]
+print '25. Quiz: LAY received     :',enron_data["LAY KENNETH L"]["total_payments"]
+print '25. Quiz: FASTOW received  :',enron_data["FASTOW ANDREW S"]["total_payments"]
+
+# Q27a How many folks in this dataset have a quantified salary?
+count = 0
+for person in enron_data:
+    if enron_data[person]["salary"]<>"NaN":
+        count+=1
+print '27a. Quiz: Folks with a quantified salary:',count
+
+# Q27b How many folks in this dataset have a known email address?
+count = 0
+for person in enron_data:
+    if enron_data[person]["email_address"]<>"NaN":
+        count+=1
+print '27b. Quiz: Folks with a known email address:',count
 
 
+
+
+
+print 'EOF'
